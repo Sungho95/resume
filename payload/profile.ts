9e359904b0bbc2b40603382a1d738b8b63e76af1
Sungho95 <tjdgh1785@gmail.com>
2023-02-17 04:06:15 +0900

    프로필 작성

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,7 +1,8 @@
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone, faBlog } from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
-import { faGithub, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import image from '../asset/_sample.jpg';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+// import { faGithub, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import image from '../asset/sungho.jpg';
 import { IProfile } from '../component/profile/IProfile';
 
 const profile: IProfile.Payload = {
@@ -10,35 +11,40 @@ const profile: IProfile.Payload = {
   // image: 'https://resume.yowu.dev/static/image/profile_2019.png',
   image,
   name: {
-    title: 'Lorem ipsum',
-    small: '(LI)',
+    title: '박성호',
+    small: '(백엔드)',
   },
   contact: [
     {
-      title: 'foo@bar.xyz',
-      link: '#',
+      title: 'tjdgh1785@naver.com',
+      link: 'mailto:tjdgh1785@naver.com',
       icon: faEnvelope,
     },
     {
-      title: 'Please contact me by email',
+      title: '010-4198-1785',
       icon: faPhone,
       badge: true,
     },
     {
-      title: 'https://github.com',
-      link: 'https://github.com/uyu423/resume-nextjs',
+      title: 'github.com/sungho95',
+      link: 'https://github.com/sungho95',
       icon: faGithub,
     },
     {
-      link: 'https://www.facebook.com/iu.loen',
-      icon: faFacebook,
+      title: 'ittrue.tistory.com',
+      link: 'https://ittrue.tistory.com',
+      icon: faBlog,
     },
-    {
-      title: 'YouTube',
-      link: 'https://www.youtube.com/channel/UC3SyT4_WLHzN7JmHQwKQZww',
-      // icon: faRss,
-      icon: faYoutube,
-    },
+    // {
+    //   link: 'https://www.facebook.com/iu.loen',
+    //   icon: faFacebook,
+    // },
+    // {
+    //   title: 'YouTube',
+    //   link: 'https://www.youtube.com/channel/UC3SyT4_WLHzN7JmHQwKQZww',
+    //   // icon: faRss,
+    //   icon: faYoutube,
+    // },
   ],
   notice: {
     title: "This resume is a sample page of 'https://github.com/uyu423/resume-nextjs'.",
