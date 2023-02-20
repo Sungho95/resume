44c36f3b1b0eefb7482afe20016512bf59d4b89e
Sungho95 <tjdgh1785@gmail.com>
2023-02-21 00:34:42 +0900

    project modified

--- payload/index.ts
+++ payload/index.ts
@@ -1,10 +1,10 @@
 import profile from './profile';
 import introduce from './introduce';
 import skill from './skill';
-import experience from './experience';
-import openSource from './openSource';
+// import experience from './experience';
+// import openSource from './openSource';
 import project from './project';
-import presentation from './presentation';
+// import presentation from './presentation';
 import education from './education';
 import article from './article';
 import etc from './etc';
@@ -15,10 +15,10 @@ import { _global } from './_global';
 import { IProfile } from '../component/profile/IProfile';
 import { IIntroduce } from '../component/introduce/IIntroduce';
 import { ISkill } from '../component/skill/ISkill';
-import { IOpenSource } from '../component/openSource/IOpenSource';
-import { IExperience } from '../component/experience/IExperience';
+// import { IOpenSource } from '../component/openSource/IOpenSource';
+// import { IExperience } from '../component/experience/IExperience';
 import { IProject } from '../component/project/IProject';
-import { IPresentation } from '../component/presentation/IPresentation';
+// import { IPresentation } from '../component/presentation/IPresentation';
 import { IEducation } from '../component/education/IEducation';
 import { IEtc } from '../component/etc/IEtc';
 import { IFooter } from '../component/footer/IFooter';
@@ -29,10 +29,10 @@ const Payload: Payload = {
   profile,
   introduce,
   skill,
-  openSource,
-  experience,
+  // openSource,
+  // experience,
   project,
-  presentation,
+  // presentation,
   article,
   education,
   etc,
@@ -45,10 +45,10 @@ interface Payload {
   profile: IProfile.Payload;
   introduce: IIntroduce.Payload;
   skill: ISkill.Payload;
-  openSource: IOpenSource.Payload;
-  experience: IExperience.Payload;
+  // openSource: IOpenSource.Payload;
+  // experience: IExperience.Payload;
   project: IProject.Payload;
-  presentation: IPresentation.Payload;
+  // presentation: IPresentation.Payload;
   education: IEducation.Payload;
   article: IArticle.Payload;
   etc: IEtc.Payload;
--- payload/project.ts
+++ payload/project.ts
@@ -41,15 +41,73 @@ const project: IProject.Payload = {
       ],
     },
     {
-      title: 'Lorem ipsum Project',
-      startedAt: '2016-10',
-      endedAt: '2017-11',
-      where: 'Bar Co., LTD.',
+      title: '스택 오버플로우 클론 프로젝트',
+      startedAt: '2022-12',
+      endedAt: '2023-01',
+      where: '코드스테이츠 프리 프로젝트(6인 / 2주)',
       descriptions: [
-        { content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
-        { content: 'Integer massa magna, feugiat commodo arcu sed, sagittis sollicitudin tortor' },
-        { content: 'Nullam diam ex, interdum nec lacus vel, ultrices pretium sapien' },
-        { content: 'Proin bibendum non nibh eu aliquet' },
+        {
+          content: '[팀원, 백엔드] Spring Security, JWT 토큰 기반 인증',
+          weight: 'BOLD',
+          descriptions: [
+            {
+              content: 'DB 테이블 기획 및 설계  ERD, API 문서 작성',
+            },
+            {
+              content: 'Spring Security를 활용한 보안 설정',
+            },
+            {
+              content: 'JWT 토큰 기반 인증 구현',
+            },
+            {
+              content: '회원 도메인 CRUD, JavaMailSender  구현',
+            },
+            {
+              content: 'AWS(EC2, S3)를 활용한 수동 배포 구현',
+            },
+          ],
+        },
+        {
+          content: '깃허브 리포지토리',
+          href: 'https://github.com/codestates-seb/seb41_pre_029',
+        },
+      ],
+    },
+    {
+      title: '모바일 키오스크 프로젝트',
+      startedAt: '2021-03',
+      endedAt: '2021-10',
+      where: '한경대학교 졸업 프로젝트(3인)',
+      descriptions: [
+        {
+          content: '[팀원, 풀스택] Spring Boot, Spring MVC, Thymeleaf, HTML, CSS, AWS(EC2, RDS)',
+          weight: 'BOLD',
+          descriptions: [
+            {
+              content: '프로젝트 아이디어, DB 테이블, 엔티티 기획 및 설계',
+            },
+            {
+              content: '상품, 주문 도메인 CRUD 구현',
+            },
+            {
+              content: 'Thymeleaf를 활용한 페이지 렌더링',
+            },
+            {
+              content: 'Bootstrap을 활용한 판매 페이지 및 고객 페이지 구현',
+            },
+            {
+              content: 'AWS(EC2, RDS)를 활용한 수동 배포 구현',
+            },
+          ],
+        },
+        {
+          content: '경진대회 전시 링크',
+          href: 'https://www.vavafair.com/home/booth/main/mobilekiosk',
+        },
+        {
+          content: '깃허브 리포지토리',
+          href: 'https://github.com/Sungho95/Mobile-Kiosk',
+        },
       ],
     },
   ],
