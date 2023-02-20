b71977dc2bd29c66653fea77109a7e302f3df3d0
Sungho95 <tjdgh1785@gmail.com>
2023-02-21 00:03:26 +0900

    project test

--- payload/project.ts
+++ payload/project.ts
@@ -4,11 +4,32 @@ const project: IProject.Payload = {
   disable: false,
   list: [
     {
-      title: 'Mashup API Development',
-      startedAt: '2018-02',
-      where: 'Foo Company',
+      title: '인디고 프로젝트',
+      startedAt: '2023-01',
+      endedAt: '2023-02',
+      where: '코드스테이츠 메인 프로젝트',
       descriptions: [
-        { content: 'Development and operation of Mashup API of Aggregation Layer' },
+        {
+          content: '[팀장, 백엔드] Redis, Spring Batch, 배포(AWS, Github Actions)',
+          weight: 'BOLD',
+          descriptions: [
+            {
+              content: '프로젝트 아이디어, DB 테이블 기획 및 설계  ERD, API 문서 작성',
+            },
+            {
+              content: 'Redis와 Spring Batch를 활용한 쿼리 최적화',
+            },
+            {
+              content: 'QueryDSL을 활용한 동적 검색 쿼리 구현',
+            },
+            {
+              content: 'Github Actions, AWS(EC2, S3, CodeDeploy)를 활용한 배포 자동화 구현',
+            },
+            {
+              content: '커뮤니티 게시판 CRUD, JavaMailSender 구현',
+            },
+          ],
+        },
         {
           content: 'Add Foo Feature in 2019',
           weight: 'MEDIUM',
