0c23b64f0574f7def272dc38acad86a333de771d
Sungho95 <tjdgh1785@gmail.com>
2023-04-10 22:45:06 +0900

    add Experience

--- payload/experience.ts
+++ payload/experience.ts
@@ -4,17 +4,16 @@ const experience: IExperience.Payload = {
   disable: false,
   disableTotalPeriod: false,
   list: [
-    // {
-    //   title: 'Foo Company',
-    //   position: 'Node.js Developer',
-    //   startedAt: '2018-02',
-    //   descriptions: [
-    //     'OTA Service Provider',
-    //     'Data modeling and Mashup API development',
-    //     'Systems Engineering and DevOps',
-    //   ],
-    //   skillKeywords: ['Node.js', 'TypeScript', 'Express.js', 'Mashup API', 'AWS'],
-    // },
+    {
+      title: '키트웍스 (Kit Works)',
+      position: '백엔드 개발자',
+      startedAt: '2023-04',
+      descriptions: [
+        'IT 솔루션 및 스터디 모아 개발/운영 스타트업',
+        '현대자동차 캐스퍼 백엔드 개발',
+      ],
+      skillKeywords: ['Java', 'Spring', 'Mybatis', 'Tibero', 'Jenkins'],
+    },
     // {
     //   title: 'Bar Co., Ltd.',
     //   position: 'Software Engineer',
